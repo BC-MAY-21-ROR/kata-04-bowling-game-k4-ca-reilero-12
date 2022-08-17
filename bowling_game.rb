fc5f902c82238ac6bd92a9eab38a442c013cf148
willcab <willianeduardocg13@gmail.com>
2022-08-17 17:56:46 -0500

    add methods for Game class
    Co-authored-by: JorgeRodz <jarf4321@gmail.com>
    Co-authored-by: jorgefloresv75 <jorgefloresv75@yahoo.es>

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,13 +1,35 @@
 # frozen_string_literal: true
 
-class Score
-
-end
-
-
 class Game
   def initialize
-    @frames = []
+    @frames = Array.new(10){initialize_frames} # [[roll1, roll2, score_total],[roll1, roll2, score_total]..]
+  end
+
+  def frames
+    @frames
+  end
+
+  def initialize_frames()
+    [roll(),state(),0]
+  end
+
+  def roll(roll,roll2=0)
+    pins=10
+  end
+
+  def state (roll,roll2=0)
+    pins=10
+    pins-= (roll+roll2)
+    :strike if roll2==0 && pins==0
+    :spare if pins==0
+    :normal
+  end
+
+  def spare()
+  end
+
+  def strike()
+    roll(next_frame)
   end
 end
 
